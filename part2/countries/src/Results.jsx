577c849fcd1d66f3c1d43881b4c9f48ffc2a7ffb
danodoms <danodoms@gmail.com>
2024-02-11 08:53:44 +0800

    saving progress

--- part2/countries/src/Results.jsx
+++ part2/countries/src/Results.jsx
@@ -1,18 +1,24 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 
 const Results = ({ searchTerm, allCountries }) => {
   console.log("allCountries: ", allCountries);
 
+  const [results, setResults] = useState(null);
+  const [weather, setWeather] = useState(null);
+
   useEffect(() => {
     generateResults();
   }, [searchTerm]);
 
+  useEffect(() => {
+    generateWeatherContent();
+  }, [weather]);
+
   const filteredCountries = allCountries.filter((country) => {
     return country.name.common.toLowerCase().includes(searchTerm.toLowerCase());
   });
-
-  const [results, setResults] = useState(null);
 
   const generateResults = () => {
     if (filteredCountries.length === 1) {
@@ -51,8 +57,40 @@ const Results = ({ searchTerm, allCountries }) => {
           ))}
         </ul>
         <img src={country.flags.png} alt="" />
+
+        <div>{generateWeatherContent(country)}</div>
       </>
     );
+  };
+
+  const generateWeatherContent = (country) => {
+    console.log("country weather: ", country);
+    const lat = country.latlng[0];
+    const lon = country.latlng[1];
+    const apiKey = "db218f43a8bd252a3828ac6d9019d4d1";
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setWeather(response.data);
+      })
+      .catch((error) => console.log(error));
+
+    console.log("weather: ", weather);
+
+    if (weather === null) {
+      return <p>Fetching weather...</p>;
+    } else {
+      return (
+        <>
+          <h2>Weather in {country.capital}</h2>
+          <p>temperature {weather.main.temp - 273.15}</p>
+        </>
+      );
+    }
   };
 
   console.log("filteredCountries: ", filteredCountries);
